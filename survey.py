bc44d830d6756834a4d3477646fd681b0bd84677
Chodavarapu Phani Visweswara Sandeep <46485155+phanivsandeep@users.noreply.github.com>
2020-07-03 23:46:00 +0530

    first commit

---
+++ survey.py
@@ -0,0 +1,104 @@
+import PyPDF2
+import re
+num="1234567890"
+contentInPage=[]
+questions=[]
+option1,option2,option3,option4=[],[],[],[]
+answers=[]
+class exam:
+    def pdfToText(self):
+        pdfObject=open('The_Living_World.pdf','rb')
+        reader=PyPDF2.PdfFileReader(pdfObject)
+        numOfPages=reader.numPages
+        for i in range(numOfPages):
+            contentInPage.append(list((reader.getPage(i).extractText()).split("\n")))
+        pdfObject.close()
+    def clearGarbageAndSolution(self):
+        for i in contentInPage:
+            gv=i.index("10005 Ph.011-47623456")
+            i=i[gv+1:]
+            for j in i:
+                if "Sol.Answer" in j:#Extracting answers here
+                    for k in range(len(j)):
+                        if j[k]=="(":
+                            answers.append(j[k+1])
+                            break
+                if "[" in j:#Removing the text like aieee, aipmt etc
+                    start=j.index("[")
+                    end=j.index("]")+1
+                    temp=j[start:end]
+                    j=j.replace(temp,"")
+                if "(1)" or "(2)" or "(3)" or "(4)" in j:#Extraction of options
+                    if "(1)" in j and "Sol.Answer" not in j:
+                        start=j.index("1")
+                        end=0
+                        if "(2)" in j:
+                            end=j.index("2")-1
+                        else:
+                            end=len(j)
+                        opt1=j[start:end]
+                        if len(opt1)>2 and j[start+1]==")":
+                            option1.append(opt1)
+                    if "(2)" in j and "Sol.Answer" not in j:
+                        start=j.index("2")
+                        end=0
+                        if "(3)" in j:
+                            end=j.index("3")-1
+                        else:
+                            end=len(j)
+                        opt2=j[start:end]
+                        if len(opt2)>2 and j[start+1]==")":
+                            option2.append(opt2)
+                    if "(3)" in j and "Sol.Answer" not in j:
+                        start=j.index("3")
+                        end=0
+                        if "(4)" in j:
+                            end=j.index("4")-1
+                        else:
+                            end=len(j)
+                        opt3=j[start:end]
+                        if len(opt3)>2 and j[start+1]==")":
+                            option3.append(opt3)
+                    if "(4)" in j:
+                        start=j.index("4")
+                        end=0
+                        if "Sol.Answer" in j:
+                            end=j.index(".")-3
+                        else:
+                            end=len(j)
+                        opt4=j[start:end]
+                        if len(opt3)>2 and j[start+1]==")":
+                            option3.append(opt4)
+                if len(j)>1:
+                    temp=re.findall("[123456789]",j)
+                    for y in temp:
+                        try:
+                            if j[j.index(y)+1]=="." or j[j.index(y)+2]==".":
+                                questions.append(j[j.index(y):])
+                                break
+                        except:
+                            pass    
+        
+            
+    
+if __name__=="__main__":
+    st1=exam()
+    st1.pdfToText()
+    st1.clearGarbageAndSolution()
+    file1=open("questions.txt","a")
+    file1.write("\n".join(questions))
+    file1.close()
+    file1=open("answers.txt","a")
+    file1.write("\n".join(answers))
+    file1.close()
+    file1=open("multiChoice.txt","a")
+    file1.write("All option (1)")
+    file1.write("\n".join(option1))
+    file1.write("All option (2)")
+    file1.write("\n".join(option2))
+    file1.write("All option (3)")
+    file1.write("\n".join(option3))
+    file1.write("All option (4)")
+    file1.write("\n".join(option4))
+    file1.close()
+    
